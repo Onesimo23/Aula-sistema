77c8dcaca58ac7118d01f4d4e4881dbbe62c130e
José Onésimo Abel Nuvunga <onesimonuvunga@gmail.com>
2024-04-20 14:01:19 +0200

    addcourse

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,31 @@ class UserController extends Controller
 {
     public function index()
     {
-        //
         $users = User::all();
-
-       
       return view('user.index', compact('users'));
        
+    }
+    public function create()
+    {
+      return view('user.create');
+    }
+    public function store(Request $request)
+    {
+        
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+            
+        ]);
+
+        
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),          
+        ]);
+
+        return redirect()->route('user')->with('success', 'Usuário criado com sucesso!');
     }
 }
---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,44 @@
+@extends('main')
+
+@section('content')
+<div class="page-content">
+
+
+<div class="container">
+
+         
+    <h1>Criar Novo Usuário</h1>
+
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf
+
+        <div>
+            <label for="name">Nome:</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}" required>
+        </div>
+
+        <div>
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" value="{{ old('email') }}" required>
+        </div>
+
+        <div>
+            <label for="password">Senha:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+        <div>
+            <button type="submit">Criar Usuário</button>
+        </div>
+    </form>
+@endsection
